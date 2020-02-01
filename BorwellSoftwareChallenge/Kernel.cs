829da08823b8ba3575646ade3feca0e92ffa6d83
Garb18 <46030758+Garb18@users.noreply.github.com>
2020-02-01 12:44:43 +0000

    Add files via upload

---
+++ BorwellSoftwareChallenge/Kernel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BorwellSoftwareChallenge.Interfaces;
+
+namespace BorwellSoftwareChallenge
+{
+    class Kernel
+    {
+        IAreaCalculator _areaCalculator;
+        IVolumeCalculator _volumeCalculator;
+        IParseDecimalFromInput _decimalParse;
+
+        public Kernel()
+        {
+            //Instantiate area calculator
+            _areaCalculator = new AreaCalculator();
+            _volumeCalculator = new VolumeCalculator();
+            _decimalParse = new ParseDecimalFromInput();
+        }
+
+        public void Run()
+        {
+            Console.Write("Please input the length of your room in metres: ");
+
+            string input = Console.ReadLine();
+            decimal length = _decimalParse.ParseDecimal(input);
+            
+            Console.Write("Please input the width of your room in metres: ");
+
+            input = Console.ReadLine();
+            decimal width = _decimalParse.ParseDecimal(input);
+            
+            Console.Write("Please input the height of your room in metres: ");
+
+            input = Console.ReadLine();
+            decimal height = _decimalParse.ParseDecimal(input);
+
+
+            Console.Write("Please input the area of your doors in m²: ");
+
+            input = Console.ReadLine();
+            decimal doorArea = _decimalParse.ParseDecimal(input);
+
+
+            Console.Write("Please input the area of your windows in m²: ");
+
+            input = Console.ReadLine();
+            decimal windowArea = _decimalParse.ParseDecimal(input);
+
+            decimal area = _areaCalculator.CalculateArea(width, length);
+            decimal trueArea = area - (doorArea + windowArea);
+            decimal volume = _volumeCalculator.CalculateVolume(width, length, height);
+
+            Console.WriteLine("The total area of your room is: {0}m²", area);
+            Console.WriteLine("The true area of your room is: {0}m²", trueArea);
+            Console.WriteLine("The volume of your room is: {0}m³", volume);
+        }
+    }
+}
